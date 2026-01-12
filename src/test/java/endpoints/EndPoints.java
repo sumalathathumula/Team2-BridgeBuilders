03de26bef201d7e092113a7c5eff16ce48795672
preethi19mit <preethi19mit@gmail.com>
2026-01-12 15:19:13 -0500

    Program added

--- src/test/java/endpoints/EndPoints.java
+++ src/test/java/endpoints/EndPoints.java
@@ -4,8 +4,30 @@ public enum EndPoints {
 	
 	//Login end points
 		USER_SIGN_IN("/login"),
+		
+		//Program end points 
+		CREATE_PROGRAM("/saveprogram"),
+		
+		GET_ALL_PROGRAMS("/allPrograms"),
+		
+		GET_PROGRAM_BYPROGRAMID("/programs/{programId}"),
+		
+		GET_ALLPROGRAMS_WITHUSERS("/allProgramsWithUsers"),
+		
+		UPDATE_PROGRAM_BYPROGRAMNAME("/program/{programName}"),
+		
+		UPDATE_PROGRAM_BYPROGRAMID("/putprogram/{programId}"),
+		
+		DELETE_PROGRAM_BYPROGRAMID("/deletebyprogid/{programId}"),
+		
+		DELETE_PROGRAM_BYPROGRAMNAME("/deletebyprogname/{programName}"),
+		
 		//user end points
 		CREATE_USER("/users/roleStatus");
+	
+	
+	
+	
 	
 	private String endpoint;
 	private static final String baseUrl = "https://lms-hackthon-feb25-803334c87fbe.herokuapp.com/lms";
