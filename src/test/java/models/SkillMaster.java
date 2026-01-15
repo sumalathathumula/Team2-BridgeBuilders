16e2d88fa04dfc4bccce0b78ce2a0b40eaca4c12
kalaisathya <sathya.kalaisathy7@gmail.com>
2026-01-15 11:16:29 -0500

    Added models file

---
+++ src/test/java/models/SkillMaster.java
@@ -0,0 +1,13 @@
+package models;
+
+public class SkillMaster {
+    private String skillName;
+
+    public String getSkillName() {
+        return skillName;}
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+}
+
