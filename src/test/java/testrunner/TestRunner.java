d486e81013e83975a20f37b9243b7a6aeea923ab
preethi19mit <preethi19mit@gmail.com>
2026-01-16 13:28:46 -0500

    program

--- src/test/java/testrunner/TestRunner.java
+++ src/test/java/testrunner/TestRunner.java
@@ -4,9 +4,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-			features = {"src/test/resources/Features"}, 
-
+			features = {"src/test/resources/Features"},
 			glue = {"stepDefinitions"},	
+			tags = "@admin-login or @Program",
 		 // tags ="@login  or @ProgramModule or @batch or @GetBatches or @GetBatchByBatchId or  @UpdateBatchByBatchId or @GetBatchByBatchName or ",
 			plugin = {"pretty", "html:target/Team1_LMSReport.html","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 
